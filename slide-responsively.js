3483364ac5395934da54dd33316cd29fceefc1a9
tracey <tracey@minnie.local>
2015-05-23 22:54:55 -0700

    comments

--- slide-responsively.js
+++ slide-responsively.js
@@ -1,6 +1,6 @@
 /**************************************************************
 
-  slide-responsibly:
+  slide-responsively:
   Inspired by wordpress "sliding-door" theme.
   Rewritten entirely to be responsive! 
   (and using minimal jQuery, not mootools)
@@ -13,10 +13,12 @@
   $(document).ready(function(){
     // DOM ready, page still (likely) loading images, etc...
     $('.slide-responsively li').mouseenter(function(e){
+      // user has hovered over a nav img.  (class) mark it "in" and the others "out"
       $('.slide-responsively li').addClass('out');
       $(this).removeClass('out').addClass('in');
     });
     $('.slide-responsively li').mouseleave(function(e){
+      // user has stopped hovering.  remove all "out", then "in", class markers.
       $('.slide-responsively li').removeClass('out');    
       $(this).removeClass('in');
     });
