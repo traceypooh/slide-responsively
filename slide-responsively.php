848fdeeef7cb36f272b2d8a795c6adaabae81498
tracey <tracey@minnie.local>
2015-05-23 22:14:33 -0700

    switched to better image urls.  added instructions on how to INSTALL and CUSTOMIZE

--- slide-responsively.php
+++ slide-responsively.php
@@ -10,11 +10,21 @@ License: GPL2
 */
 ?><?php
 
-// http://codex.wordpress.org/Plugin_API/Action_Reference
 
 // Make sure we don't expose any info if called directly
 if (!function_exists('add_action'))  exit;
-  
+
+
+// INSTALL:
+//   You can install this plugin by git cloning the repository into your /wp-content/plugins/ directory.
+//   Then go to your WP site "dashboard" and "Activate" the "slide-responsively" plugin which you should see there.
+//
+// CUSTOMIZE:
+// simply edit the urls and image locations below by going to your WP site "dashboard"
+// and find the left-side "Plugins" and hit the "Editor" link below it.
+// Find the "Select plugin to edit:" in upper right, select "slide-responsively", hite [Select] button.
+// You can then update the images and links to what you like.
+// Enjoy!
   
 
 add_filter('template_include', function($str){
@@ -23,17 +33,17 @@ add_filter('template_include', function($str){
   
   ?>
   <div>
-    <a href="/"><img src="/images/nav/poohbot-header-by.jpg"></a>
+    <a href="/"><img src="http://poohBot.com/images/nav/poohbot-header-by.jpg"></a>
   </div>
   <div class="slide-responsively">
     <ul>
-      <li><a href="/about/"     style="background-image:url(/wp-content/uploads/2009/03/about.jpg)     ">About</a></li>
-      <li><a href="/biking/"    style="background-image:url(/wp-content/uploads/2010/06/biking.jpg)    ">biking</a></li>
-      <li><a href="/favorites/" style="background-image:url(/wp-content/uploads/2010/06/favorites1.jpg)">favorites</a></li>
-      <li><a href="/quotes/"    style="background-image:url(/wp-content/uploads/2010/06/quotes.jpg)    ">quotes</a></li>
-      <li><a href="/lapses/"    style="background-image:url(/wp-content/uploads/2010/06/timelapses.jpg)">Tracey sees through time</a></li>
-      <li><a href="/video/"     style="background-image:url(/wp-content/uploads/2010/06/video.jpg)     ">Video</a></li>
-      <li><a href="/photos/"    style="background-image:url(/wp-content/uploads/2010/06/photos.jpg)    ">Photos</a></li>
+      <li><a href="/about/"     style="background-image:url(http://poohBot.com/images/nav/about.jpg      ">About</a></li>
+      <li><a href="/biking/"    style="background-image:url(http://poohBot.com/images/nav/biking.jpg)    ">biking</a></li>
+      <li><a href="/favorites/" style="background-image:url(http://poohBot.com/images/nav/favorites.jpg)">favorites</a></li>
+      <li><a href="/quotes/"    style="background-image:url(http://poohBot.com/images/nav/quotes.jpg)   ">quotes</a></li>
+      <li><a href="/lapses/"    style="background-image:url(http://poohBot.com/images/nav/timelapses.jpg)">Tracey sees through time</a></li>
+      <li><a href="/video/"     style="background-image:url(http://poohBot.com/images/nav/video.jpg)     ">Video</a></li>
+      <li><a href="/photos/"    style="background-image:url(http://poohBot.com/images/nave/photos.jpg)   ">Photos</a></li>
     </ul>
   </div>
   <?php
